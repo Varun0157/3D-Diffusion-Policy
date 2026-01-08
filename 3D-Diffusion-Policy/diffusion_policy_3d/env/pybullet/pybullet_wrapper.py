7980c5300d7df5e0827f9588f1b3c6b350aea446
Varun Edachali <varun.edachali@gmail.com>
2026-01-09 00:43:49 +0530

    remove joint prediction debug

--- 3D-Diffusion-Policy/diffusion_policy_3d/env/pybullet/pybullet_wrapper.py
+++ 3D-Diffusion-Policy/diffusion_policy_3d/env/pybullet/pybullet_wrapper.py
@@ -399,26 +399,26 @@ class UR5PickPlaceEnv(gym.Env):
 
         actual_joint_positions = self.robot.get_joint_positions()
         
-        print("\n=== Joint Prediction Debug ===")
-
-        for i, (q_curr, dq, q_tgt, q_act) in enumerate(
-            zip(
-                current_joint_positions,
-                joint_deltas,
-                target_joint_positions,
-                actual_joint_positions,
-            )
-        ):
-            print(
-                f"Joint {i:02d} | "
-                f"curr: {q_curr:+.4f} | "
-                f"Δpred: {dq:+.4f} | "
-                f"target: {q_tgt:+.4f} | "
-                f"actual: {q_act:+.4f} | "
-                f"err: {(q_act - q_tgt):+.4f}"
-            )
-
-        print("==============================\n")
+        # print("\n=== Joint Prediction Debug ===")
+        #
+        # for i, (q_curr, dq, q_tgt, q_act) in enumerate(
+        #     zip(
+        #         current_joint_positions,
+        #         joint_deltas,
+        #         target_joint_positions,
+        #         actual_joint_positions,
+        #     )
+        # ):
+        #     print(
+        #         f"Joint {i:02d} | "
+        #         f"curr: {q_curr:+.4f} | "
+        #         f"Δpred: {dq:+.4f} | "
+        #         f"target: {q_tgt:+.4f} | "
+        #         f"actual: {q_act:+.4f} | "
+        #         f"err: {(q_act - q_tgt):+.4f}"
+        #     )
+        #
+        # print("==============================\n")
 
 
         for _ in range(50):
